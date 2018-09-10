dccd5b18dc07d4b3f857bddf172a93031a0a16c8
Thor <thordoor89@hotmail.com>
2018-09-10 17:42:10 +0200

    initial setup of webpack done. babel loader 7 added as well.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-    
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
+    context: path.resolve(__dirname, "src"),
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "./assets/js/[chunkhash].bundle.js",
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({tempalte:'../index.html'}),
+    ],
+    rules: [
+        {
+            test: /\.js$/,
+            use:{
+                loader: "babel-loader",
+            },
+        },
+    ],
 }
+module.exports = config;
